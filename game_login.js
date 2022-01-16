737e89d1e1be22008484e2aec80754f0809346d4
Moonlightcrush12 <87609552+Moonlightcrush12@users.noreply.github.com>
2022-01-16 16:25:02 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+function adduser(){
+
+  player_1=document.getElementById("player_1_name").value
+  player_2=document.getElementById("player_2_name").value
+
+  localStorage.setItem("player_1_name", player_1)
+  localStorage.setItem("player_2_name", player_2)
+
+  window.location="game_page.html"
+}
